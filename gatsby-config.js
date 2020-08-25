a3084cc392fef0cc0217d0ae2cd5053f71451ff0
Jasmin Bissig <jasmin.bissig@gmail.com>
2020-08-25 18:24:46 +0200

    Install react helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Atelier für Digitalstoffe",
+    description: "Interdiszipliär Neues entdecken in Kunst und Technik",
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [`gatsby-plugin-react-helmet`],
 }
