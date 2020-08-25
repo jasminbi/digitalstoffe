65c470345a52e99f5c05d0a4a1ea5d5fa5b2e631
Jasmin Bissig <jasmin.bissig@gmail.com>
2020-08-25 17:29:22 +0200

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import gif from "../gifs/digitalstoffe_pulse.gif"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <img src={gif} alt="digitalstoffe_logo" />
+      <p>Bald sind wir da.</p>
+      <a href="mailto:hallo@digitalstoffe.ch">hallo@digitalstoffe.ch</a>
+    </div>
+  )
 }
