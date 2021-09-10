dc093ad9752bed64164a822123dcbefa02f68863
Jasmin Bissig <jasmin.bissig@gmail.com>
2021-09-10 21:22:31 +0200

    Add pink sketch

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,22 +1,23 @@
 export default function Sketch(p5) {
-    let canvas;
+  let canvas
 
-    p5.setup = () => {
-      canvas = p5.createCanvas(400, 400);
-      p5.noStroke();
-    };
-
-    p5.draw = () => {
-      let y = p5.color("coral");
-
-      p5.fill(y);
-      p5.circle(45, 45, 65);
-
-      if (p5.mouseIsPressed) {
-        p5.fill(p5.color("lightsalmon"));
-      } else {
-        p5.fill(p5.color("hsl(160, 100%, 50%)"));
-      }
-      p5.circle(p5.mouseX, p5.mouseY, 70, 70);
-    };
+  p5.setup = () => {
+    canvas = p5.createCanvas(p5.displayWidth, p5.displayHeight)
+    canvas.position(0, 0).style("z-index", "-1")
+    p5.noStroke()
   }
+
+  p5.draw = () => {
+    let y = p5.color("#FF00FF")
+
+    p5.fill(y)
+    p5.circle(60, 60, 0)
+
+    // if (p5.mouseIsPressed) {
+    //   p5.fill(p5.color("#FF77FF"))
+    // } else {
+    p5.fill(p5.color("#FF00FF"))
+    // }
+    p5.circle(p5.mouseX, p5.mouseY, 60, 60)
+  }
+}
